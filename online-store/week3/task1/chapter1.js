d9638c6546addd18c99a13eb150b382b8de7fb9a
User224839 <serikbajn18@gmail.com>
2026-02-14 15:27:11 +0500

    week5 but 4

--- online-store/week3/task1/chapter1.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-let admin;
-let name = 'John';
-
-admin = name;
-console.log(admin);
--- online-store/week3/task1/chapter2.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-const value = '10';
-
-console.log(typeof value);
-console.log(Number(value));
